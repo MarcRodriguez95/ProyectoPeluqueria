8d828672830270ea9af9c8ab56ea4c200196138a
Marc <marc.rodriguez451@gmail.com>
2017-03-27 21:02:49 +0200

    first commit

---
+++ src/Trascastro/UserBundle/Form/UserBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Trascastro\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserBundle extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getName()
+    {
+        return 'user_bundle_user_bundle';
+    }
+}
